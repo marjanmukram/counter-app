46f1e8bb06f9d53920a21ff1e17b495a8d338b82
Marjan Mukram <marjanmukram8@gmail.com>
2021-05-17 23:32:42 +0530

    formatted

--- src/components/counter/counter.jsx
+++ src/components/counter/counter.jsx
@@ -1,79 +1,192 @@
 import React from "react";
-import ClearIcon from '@material-ui/icons/Clear';
-import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
-import RemoveCircleOutlineIcon from '@material-ui/icons/RemoveCircleOutline';
+import ClearIcon from "@material-ui/icons/Clear";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
+import RemoveCircleOutlineIcon from "@material-ui/icons/RemoveCircleOutline";
 import { Button } from "@material-ui/core";
-import { makeStyles } from '@material-ui/core/styles';
-import DeleteIcon from '@material-ui/icons/Delete';
+import { makeStyles } from "@material-ui/core/styles";
+import DeleteIcon from "@material-ui/icons/Delete";
 
+const zero = makeStyles(() => ({
+  zButton: {
+    color: "white",
+    padding: "20px",
+    border: "3px solid black",
+    backgroundColor: "liteblue",
+    marginLeft: "200px"
+  }
+}));
 
-const zero = makeStyles((theme) => ({
-    zButton: {
-        color: "white",
-        padding:"20px",
-        border: "3px solid black",
-        backgroundColor: "liteblue",
-        marginLeft: "200px"
-        
-    },
-    }));
-
-const plusMinusDelete = makeStyles((theme) => ({
-    pmdButton: {
-        color: "white",
-        padding:"20px",
-        border: "3px solid black",
-        backgroundColor: "litepink",
-        marginLeft: "20px"
-        
-    },
-    }));
-      
-
+const plusMinusDelete = makeStyles(() => ({
+  pmdButton: {
+    color: "white",
+    padding: "20px",
+    border: "3px solid black",
+    backgroundColor: "litepink",
+    marginLeft: "20px"
+  }
+}));
 
 export default function IconLabelButtons() {
-    const zClasses = zero();
-    const pmdClasses = plusMinusDelete();
-    
-    return (
+  const zClasses = zero();
+  const pmdClasses = plusMinusDelete();
+
+  return (
     <div>
-        <div>
-        <Button variant="contained" color="secondary" className={pmdClasses.pmdButton} startIcon={<ClearIcon />}> </Button>
-        <Button variant="contained" color="secondary" className={pmdClasses.pmdButton} startIcon={<DeleteIcon />}> </Button>
-        </div>
-        <div>
-        <Button DeleteOutlineSharpIcon variant='contained' color="primary" className={zClasses.zButton} >ZERO</Button>
-        <Button variant="contained" color="secondary" className={pmdClasses.pmdButton} startIcon={<AddCircleOutlineIcon />}> </Button>
-        <Button variant="contained" color="secondary" className={pmdClasses.pmdButton} startIcon={<RemoveCircleOutlineIcon />}> </Button>
-        <Button variant="contained" color="secondary" className={pmdClasses.pmdButton} startIcon={<DeleteIcon />}> </Button>
-        </div>,
+      <div>
+        <Button
+          variant='contained'
+          color='secondary'
+          className={pmdClasses.pmdButton}
+          startIcon={<ClearIcon />}
+        >
+          {" "}
+        </Button>
+        <Button
+          variant='contained'
+          color='secondary'
+          className={pmdClasses.pmdButton}
+          startIcon={<DeleteIcon />}
+        >
+          {" "}
+        </Button>
+      </div>
+      <div>
+        <Button
+          DeleteOutlineSharpIcon
+          variant='contained'
+          color='primary'
+          className={zClasses.zButton}
+        >
+          ZERO
+        </Button>
+        <Button
+          variant='contained'
+          color='secondary'
+          className={pmdClasses.pmdButton}
+          startIcon={<AddCircleOutlineIcon />}
+        >
+          {" "}
+        </Button>
+        <Button
+          variant='contained'
+          color='secondary'
+          className={pmdClasses.pmdButton}
+          startIcon={<RemoveCircleOutlineIcon />}
+        >
+          {" "}
+        </Button>
+        <Button
+          variant='contained'
+          color='secondary'
+          className={pmdClasses.pmdButton}
+          startIcon={<DeleteIcon />}
+        >
+          {" "}
+        </Button>
+      </div>
+      <div>
+        <Button
+          DeleteOutlineSharpIcon
+          variant='contained'
+          color='primary'
+          className={zClasses.zButton}
+        >
+          ZERO
+        </Button>
+        <Button
+          variant='contained'
+          color='secondary'
+          className={pmdClasses.pmdButton}
+          startIcon={<AddCircleOutlineIcon />}
+        >
+          {" "}
+        </Button>
+        <Button
+          variant='contained'
+          color='secondary'
+          className={pmdClasses.pmdButton}
+          startIcon={<RemoveCircleOutlineIcon />}
+        >
+          {" "}
+        </Button>
+        <Button
+          variant='contained'
+          color='secondary'
+          className={pmdClasses.pmdButton}
+          startIcon={<DeleteIcon />}
+        >
+          {" "}
+        </Button>
+      </div>
+      <div>
+        <Button
+          DeleteOutlineSharpIcon
+          variant='contained'
+          color='primary'
+          className={zClasses.zButton}
+        >
+          ZERO
+        </Button>
+        <Button
+          variant='contained'
+          color='secondary'
+          className={pmdClasses.pmdButton}
+          startIcon={<AddCircleOutlineIcon />}
+        >
+          {" "}
+        </Button>
+        <Button
+          variant='contained'
+          color='secondary'
+          className={pmdClasses.pmdButton}
+          startIcon={<RemoveCircleOutlineIcon />}
+        >
+          {" "}
+        </Button>
+        <Button
+          variant='contained'
+          color='secondary'
+          className={pmdClasses.pmdButton}
+          startIcon={<DeleteIcon />}
+        >
+          {" "}
+        </Button>
+      </div>
+      <div>
+        <Button
+          DeleteOutlineSharpIcon
+          variant='contained'
+          color='primary'
+          className={zClasses.zButton}
+        >
+          ZERO
+        </Button>
+        <Button
+          variant='contained'
+          color='secondary'
+          className={pmdClasses.pmdButton}
+          startIcon={<AddCircleOutlineIcon />}
+        >
+          {" "}
+        </Button>
+        <Button
+          variant='contained'
+          color='secondary'
+          className={pmdClasses.pmdButton}
+          startIcon={<RemoveCircleOutlineIcon />}
+        >
+          {" "}
+        </Button>
+        <Button
+          variant='contained'
+          color='secondary'
+          className={pmdClasses.pmdButton}
+          startIcon={<DeleteIcon />}
+        >
+          {" "}
+        </Button>
+      </div>
+    </div>
+  );
+}
 
-        <div>
-        <Button DeleteOutlineSharpIcon variant='contained' color="primary" className={zClasses.zButton} >ZERO</Button>
-        <Button variant="contained" color="secondary" className={pmdClasses.pmdButton} startIcon={<AddCircleOutlineIcon />}> </Button>
-        <Button variant="contained" color="secondary" className={pmdClasses.pmdButton} startIcon={<RemoveCircleOutlineIcon />}> </Button>
-        <Button variant="contained" color="secondary" className={pmdClasses.pmdButton} startIcon={<DeleteIcon />}> </Button>
-        </div>,
-
-        <div>
-        <Button DeleteOutlineSharpIcon variant='contained' color="primary" className={zClasses.zButton} >ZERO</Button>
-        <Button variant="contained" color="secondary" className={pmdClasses.pmdButton} startIcon={<AddCircleOutlineIcon />}> </Button>
-        <Button variant="contained" color="secondary" className={pmdClasses.pmdButton} startIcon={<RemoveCircleOutlineIcon />}> </Button>
-        <Button variant="contained" color="secondary" className={pmdClasses.pmdButton} startIcon={<DeleteIcon />}> </Button>
-        </div>,
-
-        <div>
-        <Button DeleteOutlineSharpIcon variant='contained' color="primary" className={zClasses.zButton} >ZERO</Button>
-        <Button variant="contained" color="secondary" className={pmdClasses.pmdButton} startIcon={<AddCircleOutlineIcon />}> </Button>
-        <Button variant="contained" color="secondary" className={pmdClasses.pmdButton} startIcon={<RemoveCircleOutlineIcon />}> </Button>
-        <Button variant="contained" color="secondary" className={pmdClasses.pmdButton} startIcon={<DeleteIcon />}> </Button>
-        </div>,
-
-      
-    </div>   
-    );
-};
-
-
-
-// export default Counter;
